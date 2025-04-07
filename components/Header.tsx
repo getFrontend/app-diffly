0d5194b79a69648c05785158a660a78b97d6cf75
getFrontend <sergey10goals@gmail.com>
2025-04-07 22:36:33 +0300

    add Diffly logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from "next/image";
 import { AiFillMoon } from "react-icons/ai";
 import { PiSunLight } from "react-icons/pi";
 
@@ -11,9 +12,13 @@ interface HeaderProps {
 export default function Header({ isDarkMode, onToggleDarkMode }: HeaderProps) {
   return (
     <header className="container mx-auto flex justify-between items-center p-4 border-b border-color">
-      <h1 className="text-xl font-semibold text-foreground">
-        Diffly - your simple Comparison Tool
-      </h1>
+      <div className="flex flex-row items-center gap-2 max-sm:gap-0">
+        <Image src="/logo-diffly-96.png" alt="Logo" width={32} height={32} />
+        <h1 className="text-xl font-semibold text-foreground">
+          Diffly{' '}
+          <span className="max-sm:hidden"> - your simple Comparison Tool</span>
+        </h1>
+      </div>
       <button
         onClick={onToggleDarkMode}
         className="p-2 rounded-full text-muted hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 cursor-pointer"
